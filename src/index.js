9204eaec108681404e4e0cabe9f69c801dcd6da0
j0lv3r4 <jolvera@posteo.de>
2019-01-05 21:46:49 -0600

    consolidate API and polish package.json

--- src/index.js
+++ src/index.js
@@ -1,69 +1,74 @@
-import React, { Component } from 'react'
+import { Component } from 'react'
 import Router from 'next/router'
 import nextCookie from 'next-cookies'
 import cookie from 'js-cookie'
 
-export function withAuthSync(WrappedComponent) {
-  return class Auth extends Component {
-    static async getInitialProps(ctx) {
-      const token = auth(ctx)
+export const login = ({ token, cookieOptions, redirect }) => {
+  cookie.set('token', token, cookieOptions)
+  Router.push(redirect)
+}
 
-      const componentProps =
-        WrappedComponent.getInitialProps &&
-        (await WrappedComponent.getInitialProps(ctx))
+export const logout = redirect => {
+  cookie.remove('token')
+  // to support logging out from all windows
+  window.localStorage.setItem('logout', Date.now())
+  Router.push(redirect)
+}
 
-      return { ...componentProps, token }
-    }
+export default function withAuth({ redirect }) {
+  return function withAuthFactory(WrappedComponent) {
+    return class Auth extends Component {
+      static async getInitialProps(context) {
+        const token = auth({ context, redirect })
 
-    constructor(props) {
-      super(props)
+        const componentProps =
+          WrappedComponent.getInitialProps &&
+          (await WrappedComponent.getInitialProps(context))
 
-      this.syncLogout = this.syncLogout.bind(this)
-    }
-
-    componentDidMount() {
-      window.addEventListener('storage', this.syncLogout)
-    }
-
-    componentWillUnmount() {
-      window.removeEventListener('storage', this.syncLogout)
-      window.localStorage.removeItem('logout')
-    }
-
-    syncLogout(event) {
-      if (event.key === 'logout') {
-        console.log('logged out from storage!')
-        Router.push('/login')
+        return { ...componentProps, token }
       }
-    }
 
-    render() {
-      return <WrappedComponent {...this.props} />
+      constructor(props) {
+        super(props)
+        this.syncLogout = this.syncLogout.bind(this)
+      }
+
+      componentDidMount() {
+        window.addEventListener('storage', this.syncLogout)
+      }
+
+      componentWillUnmount() {
+        window.removeEventListener('storage', this.syncLogout)
+        window.localStorage.removeItem('logout')
+      }
+
+      syncLogout(event) {
+        if (event.key === 'logout') {
+          Router.push(redirect)
+        }
+      }
+
+      render() {
+        return <WrappedComponent {...this.props} />
+      }
     }
   }
 }
 
-export const auth = (ctx, failRedirect = '/login') => {
-  const { token } = nextCookie(ctx)
+export const auth = ({ context, redirect }) => {
+  const { token } = nextCookie(context)
 
-  if (ctx.req && !token) {
-    ctx.res.writeHead(302, { Location: failRedirect })
-    ctx.res.end()
+  if (context.req && !token) {
+    context.res.writeHead(302, { Location: redirect })
+    context.res.end()
     return
   }
 
   if (!token) {
-    Router.push(failRedirect)
+    Router.push(redirect)
   }
 
   return token
-}
-
-export const logout = () => {
-  cookie.remove('token')
-  // to support logging out from all windows
-  window.localStorage.setItem('logout', Date.now())
-  Router.push('/login')
 }
 
 export const getCookie = nextCookie
